ccd06cc105af64987b11e8cb462d597aee943cf0
Joerex Thambaiah <joerexthambaiah@Joerexs-MacBook-Pro.local>
2024-05-12 18:01:52 -0400

    EchoPDF: Added the .js page responsible for displaying the google ad

---
+++ src/GoogleAds.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const GoogleAds = () => 
+{
+    return (
+        <body>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5744845104739846"crossorigin="anonymous"></script>
+        <ins class="adsbygoogle"
+            style={{display: 'inline-block', width: 336, height: 5}}
+            data-ad-client="ca-pub-5744845104739846"
+            data-ad-slot="7062279280"
+            data-adtest="on"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+        </body>
+    );
+}
+export default GoogleAds
