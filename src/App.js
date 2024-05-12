d49bc6829b0674428147cc512979c932feaeacdc
Arty2001 <athavanth5@gmail.com>
2024-05-12 17:34:46 -0400

    basic home page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { Center, Group, Text, rem } from '@mantine/core';
-import { Dropzone } from '@mantine/dropzone';
-import { IconPhoto, IconUpload, IconX } from '@tabler/icons-react';
+import React from 'react';
+import Home from './pages/Home';
 import { useEffect, useState } from 'react';
 import * as pdfjs from 'pdfjs-dist';
 import './App.css';
@@ -77,45 +76,15 @@ function App() {
   }
 
   return (
-    <Center w={'100%'} h={'100vh'}>
+    <div w={'100%'} h={'100vh'}>
       {pdfData ? (
         <div>
           {Array.from({ length: pdfData.numPages }, (_, index) => (
             <PdfPage key={index} pageNumber={index + 1} pdf={pdfData} />
           ))}
         </div>
-      ): (<Dropzone accept={['application/pdf']} onDrop={(files) => pdfToHtml(files[0])} >
-      <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
-        <Dropzone.Accept>
-          <IconUpload
-            style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }}
-            stroke={1.5}
-          />
-        </Dropzone.Accept>
-        <Dropzone.Reject>
-          <IconX
-            style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }}
-            stroke={1.5}
-          />
-        </Dropzone.Reject>
-        <Dropzone.Idle>
-          <IconPhoto
-            style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }}
-            stroke={1.5}
-          />
-        </Dropzone.Idle>
-
-        <div>
-          <Text size="xl" inline>
-            Drag images here or click to select files
-          </Text>
-          <Text size="sm" c="dimmed" inline mt={7}>
-            Attach as many files as you like, each file should not exceed 5mb
-          </Text>
-        </div>
-      </Group>
-      </Dropzone>)}
-    </Center>
+      ): (<Home pdfToHtml={pdfToHtml}/>)}
+    </div>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { DropzoneButton } from '../components/DropZoneButton/DropzoneButton';
+import Header from '../components/Header/Header';
+import { Stack } from '@mantine/core';
+import { FeaturesAsymmetrical } from '../components/FeaturesAsymmetrical/FeaturesAsymmetrical';
+
+export default function Home({pdfToHtml}){
+    return (<Stack h={'95vh'} p={'3vh'} justify='space-between'><Header/><DropzoneButton pdfToHtml={pdfToHtml}/><FeaturesAsymmetrical/></Stack>)
+}
